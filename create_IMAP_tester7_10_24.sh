214921d57d50af8539bd6cf1af693f729e333021
Zachary Robbins <zacharyinthewoods@gmail.com>
2024-07-22 07:29:59 -0600

    updated runs

---
+++ create_IMAP_tester7_10_24.sh
@@ -0,0 +1,156 @@
+#!/bin/sh
+# =======================================================================================
+#
+# This script will create, setup and build a single-site simulation for the S_CA_FATES
+# project 
+# Based off Ryan Knoxes script for BCI 
+#
+# USER SETTINGS
+# USER MAY ALSO WANT TO ADJUST XML CHANGES, AND NAMELIST ARGUMENTS
+# =====================================================================================
+export user=zjrobbins
+export TAG='IMAPtester2'                               # User defined tag to differentiate runs
+export COMPSET=IELMFATES                                  # Compset (probably ICLM45ED or ICLM50ED)
+export MAC=chicoma                                        # Name your machine
+export COMPILER=gnu                                       # Name your compiler
+export FUNDING=t24_forecast
+export FATES_PARAM=LFM_top_ten_1.nc                       # Name of FATES parameter file in S_CA_FATES/params/
+export SITE_BASE_DIR=/lustre/scratch5/.mdt1/zjrobbins/E3SM_cases/   # Where is the site folder located? (SITE_NAME)
+export DATA_DIR=/lustre/scratch5/.mdt1/zjrobbins/IMAP_SNV           # Where are the parameter and climate drivers located.
+export DIN_LOC_ROOT=/usr/projects/cesm/input_data              #location of external input data, YOU will need access to veg. 
+export CLM_USRDAT_DOMAIN=domain_SNV.nc       # Name of domain file in  $DATA_DIR/params/
+export CLM_USRDAT_SURDAT=Sierrasurf.nc # Name of surface file in $DATA_DIR/params/
+export ACME_ROOT=/lustre/scratch5/.mdt1/zjrobbins/API25_2/E3SM
+# DEPENDENT PATHS AND VARIABLES (USER MIGHT CHANGE THESE..)
+# =======================================================================================
+export CASEROOT=${SITE_BASE_DIR}                               # Where the build is generated (probably on scratch partition)
+export CLM_SURFDAT_DIR=${DATA_DIR}/Gen_Needs
+export CLM_DOMAIN_DIR=${DATA_DIR}/Gen_Needs
+export FATES_PARAM_DIR=${DATA_DIR}/params #location of FATES paameter files
+export DIN_LOC_ROOT_FORCE=${DATA_DIR}/FATES_Sierra  #location of climate forcing, not including the SITE_NAMES
+export CLM_HASH=`cd ${ACME_ROOT}/components/elm/;git log -n 1 --pretty=%h`
+export FATES_HASH=`(cd ${ACME_ROOT}/components/elm/src/external_models/fates;git log -n 1 --pretty=%h)`
+export GIT_HASH=C${CLM_HASH}-F${FATES_HASH}
+export RES=ELM_USRDAT
+export CASE_NAME=${TAG}.${COMPSET}
+
+# export USRDAT_METFORCE_NAME=${SITE_DIR} (DEPRECATED?)
+
+
+# REMOVE EXISTING CASE IF PRESENT
+rm -r ${CASEROOT}/${CASE_NAME}
+# CREATE THE CASE
+/lustre/scratch5/.mdt1/${user}/ACME_2023/E3SM/cime/scripts/create_newcase -case ${CASEROOT}/${CASE_NAME} -res ${RES} -compset ${COMPSET} -mach ${MAC} -project ${FUNDING} -compiler ${COMPILER} -mpilib="mpi-serial"
+
+cd ${CASEROOT}/${CASE_NAME} 
+
+
+# SET PATHS TO SCRATCH ROOT, DOMAIN AND MET DATA (USERS WILL PROB NOT CHANGE THESE)
+# =================================================================================
+./xmlchange --file env_run.xml --id DIN_LOC_ROOT --val ${DIN_LOC_ROOT}
+./xmlchange --file env_run.xml --id ATM_DOMAIN_FILE --val ${CLM_USRDAT_DOMAIN}
+./xmlchange --file env_run.xml --id ATM_DOMAIN_PATH --val ${CLM_DOMAIN_DIR}
+./xmlchange --file env_run.xml --id LND_DOMAIN_FILE --val ${CLM_USRDAT_DOMAIN}
+./xmlchange --file env_run.xml --id LND_DOMAIN_PATH --val ${CLM_DOMAIN_DIR}
+./xmlchange --file env_run.xml --id DATM_MODE --val CLM1PT
+./xmlchange --file env_run.xml --id ELM_USRDAT_NAME --val ${SITE_NAME}
+./xmlchange --file env_run.xml --id DIN_LOC_ROOT_CLMFORC --val ${DIN_LOC_ROOT_FORCE}
+#./xmlchange --file env_build.xml --id CESMSCRATCHROOT --val ${CASE_NAME}
+
+# SPECIFY PE LAYOUT FOR SINGLE SITE RUN (USERS WILL PROB NOT CHANGE THESE)
+# =================================================================================
+
+./xmlchange NTASKS_ATM=1
+./xmlchange NTASKS_CPL=1
+./xmlchange NTASKS_GLC=1
+./xmlchange NTASKS_OCN=1
+./xmlchange NTASKS_WAV=1
+./xmlchange NTASKS_ICE=1
+./xmlchange NTASKS_LND=1
+./xmlchange NTASKS_ROF=1
+./xmlchange NTASKS_ESP=1
+./xmlchange ROOTPE_ATM=0
+./xmlchange ROOTPE_CPL=0
+./xmlchange ROOTPE_GLC=0
+./xmlchange ROOTPE_OCN=0
+./xmlchange ROOTPE_WAV=0
+./xmlchange ROOTPE_ICE=0
+./xmlchange ROOTPE_LND=0
+./xmlchange ROOTPE_ROF=0
+./xmlchange ROOTPE_ESP=0
+./xmlchange NTHRDS_ATM=1
+./xmlchange NTHRDS_CPL=1
+./xmlchange NTHRDS_GLC=1
+./xmlchange NTHRDS_OCN=1
+./xmlchange NTHRDS_WAV=1
+./xmlchange NTHRDS_ICE=1
+./xmlchange NTHRDS_LND=1
+./xmlchange NTHRDS_ROF=1
+./xmlchange NTHRDS_ESP=1
+
+# SPECIFY RUN TYPE PREFERENCES (USERS WILL CHANGE THESE)
+# =================================================================================
+./xmlchange PIO_VERSION=2
+./xmlchange --file env_build.xml --id DEBUG --val FALSE
+./xmlchange --file env_run.xml --id STOP_N --val 20
+./xmlchange --file env_run.xml --id RUN_STARTDATE --val '2002-01-01'
+./xmlchange --file env_run.xml --id STOP_OPTION --val nyears
+./xmlchange --file env_run.xml --id REST_N --val 1
+./xmlchange --file env_run.xml --id DATM_CLMNCEP_YR_START --val 2002
+./xmlchange --file env_run.xml --id DATM_CLMNCEP_YR_END --val 2022
+
+
+# MACHINE SPECIFIC, AND/OR USER PREFERENCE CHANGES (USERS WILL CHANGE THESE)
+# =================================================================================
+
+#./xmlchange -file env_build.xml -id GMAKE -val make
+#./xmlchange -file env_run.xml -id BATCHQUERY -val ''
+#./xmlchange -file env_run.xml -id BATCHSUBMIT -val ''
+#./xmlchange -file env_run.xml -id DOUT_S_SAVE_INTERIM_RESTART_FILES -val TRUE
+#./xmlchange -file env_run.xml -id DOUT_S -val TRUE
+#./xmlchange -file env_run.xml -id DOUT_S_ROOT -val '$CASEROOT/run'
+#./xmlchange -file env_run.xml -id RUNDIR -val ${CASE_NAME}/run
+#./xmlchange -file env_build.xml -id EXEROOT -val ${CASE_NAME}/bld
+
+# MODIFY THE CLM NAMELIST (USERS MODIFY AS NEEDED)
+
+cat >> user_nl_elm <<EOF
+fsurdat = '${CLM_SURFDAT_DIR}/${CLM_USRDAT_SURDAT}'
+fates_paramfile = '${FATES_PARAM_DIR}/${FATES_PARAM}'
+use_fates_nocomp = .true.
+hist_empty_htapes = .false.
+use_fates_planthydro = .true.
+use_var_soil_thick = .false.
+use_fates_ed_st3 = .true.
+use_fates_inventory_init = .true.
+
+fates_inventory_ctrl_filename = '${CLM_SURFDAT_DIR}/Sierra_inv_file.txt'
+!fates_spitfire_mode= 1
+hist_empty_htapes = .false.
+EOF
+
+# Usefull user_nl_clm arguments: 
+# This couplet will enable hourly output
+# hist_mfilt             = 480      
+# hist_nhtfrq            = -1  
+# hist_fincl1='NEP','NPP','GPP','TLAI','TSOI_10CM','QVEGT','EFLX_LH_TOT','AR','HR','ED_biomass','ED_bleaf','ED_balive','DDBH_SCPF','BA_SCPF','NPLANT_SCPF','M1_SCPF','M2_SCPF','M3_SCPF','M4_SCPF','M5_SCPF','M6_SCPF','WIND','ZBOT','FSDS','RH','TBOT','PBOT','QBOT','RAIN','FLDS'
+
+# MODIFY THE DATM NAMELIST (DANGER ZONE - USERS BEWARE CHANGING)
+
+cat >> user_nl_datm <<EOF
+taxmode = "cycle", "cycle", "cycle"
+EOF
+
+./case.setup
+
+# HERE WE NEED TO MODIFY THE STREAM FILE (DANGER ZONE - USERS BEWARE CHANGING)
+./preview_namelists
+cp /lustre/scratch5/.mdt1/${user}/E3SM/scratch/${CASE_NAME}/run/datm.streams.txt.CLM1PT.ELM_USRDAT user_datm.streams.txt.CLM1PT.ELM_USRDAT
+#`sed -i '/FLDS/d' user_datm.streams.txt.CLM1PT.ELM_USRDAT`
+`sed -i 's|/CLM1PT_data|4cl_C1/SANGRE1_Clim|' user_datm.streams.txt.CLM1PT.ELM_USRDAT`
+
+
+
+./case.build
+
+
---
+++ ex.mp2.annual.py
@@ -0,0 +1,154 @@
+#!/usr/bin/env python
+"""
+Computes a sample using templated run, example usage:
+
+    mpirun -n 16 python parallel.run.py -c 'BCI.ICLM45ED.wolf.intel.Cb14cb81-F812a621.'  -r /lustre/scratch3/turquoise/cxu/ACME/cases  -f clm2.h0.2003-12.nc -n 1 -s 1 -t 1000 -l ./R/Missing.txt
+        
+Phillip J. Wolfram
+08/25/2017
+
+Modified by Chonggang Xu
+Feb 4,2018
+"""
+
+from mpi4py import MPI
+import numpy as np
+import pandas as pd
+import xarray as xr
+from os import path
+import os
+import argparse
+
+root='/lustre/scratch5/zjrobbins/E3SM/scratch/'
+nameofthing='IMAPtester2.IELMFATES'
+outputfolder="IMAP_tester"   # idk
+
+
+parser = argparse.ArgumentParser(description=__doc__,
+                                     formatter_class=argparse.RawTextHelpFormatter)
+parser.add_argument('-s', '--start', dest='start',
+                     help='base name of a case (casename=casebase${samplenumber+start_it', metavar='INT', required=True)
+#parser.add_argument('-r', '--runroot', dest='runroot',
+#                     help='base name of a case (casename=casebase${samplenumber+start_it', metavar='FOLDER', required=True)
+parser.add_argument('-ys', '--Yearstart', dest='Years',
+                     help='base name of a case (casename=casebase${samplenumber+start_it', metavar='INT', required=True)
+parser.add_argument('-ye', '--Yearend', dest='Yeare',
+                     help='base name of a case (casename=casebase${samplenumber+start_it', metavar='INT', required=True)
+parser.add_argument('-n', '--write', dest='write',
+                     help='base name of a case (casename=casebase${samplenumber+start_it', metavar='STRING', required=True)				 
+args = parser.parse_args()
+
+runroot=root+nameofthing
+outdir=root+outputfolder
+
+
+comm = MPI.COMM_WORLD
+rank = comm.Get_rank()
+totalprocs = comm.Get_size()
+overall = False
+if totalprocs < 2:
+    overall = True
+print('Computing using rank {} of {}'.format(rank, totalprocs))
+
+outdir=outdir
+Case=int(args.start)+int(rank)
+#Case=""
+print(Case)
+wdir=runroot#+"."+str(Case)
+#wdir=runroot
+print(wdir)
+
+
+mod='h0'
+#i=str(99)
+Yearset=list(range(int(args.Years),int(args.Yeare)+2))
+#var=['FATES_LEAF_H2OPOT_SZPF','FATES_STOMATAL_COND_AP']
+#var=['FATES_GPP','QVEGE','QVEGT','QSOIL','QRUNOFF','H2OSOI']
+var=['FATES_GPP','QVEGE','QVEGT','QSOIL','QRUNOFF','H2OSOI','FATES_LEAF_H2OPOT_SZPF',
+	'FATES_SAPFLOW','FATES_STEM_CONDFRAC_SZPF','FATES_TRANSROOT_CONDFRAC_SZPF'] #,'FATES_STEM_H2OPOT_SZPF']
+Monthset=["01","02","03","04","05","06","07","08","09","10","11","12"]
+Finishedblock=pd.DataFrame([])
+Missing=[]
+if rank ==1:
+	if os.path.isdir(outdir):
+		print("Exists")
+	else:
+		print("Doesn't exists")
+		os.mkdir(outdir)
+#print(wdir+"/run/"+wdir+"."+str(Case)+".elm.h0."+str(args.Yeare)+"-12.nc")
+
+#print(wdir+"/run/"+nameofthing+"."+str(Case)+".elm.h1."+str(args.Yeare)+"-01.nc")
+
+print(wdir+"/run/"+nameofthing+".elm.h0."+str(args.Yeare)+"-01-01-00000.nc")
+if path.exists(wdir+"/run/"+nameofthing+".elm.h0."+str(args.Yeare)+"-01-01-00000.nc")==True:
+	for Year in Yearset:
+		Year=str(Year)
+		print(Year)
+		#for month in Monthset:
+		        #print(month)
+		data_path=wdir+"/run/"+nameofthing+".elm.h0."+Year+"-01-01-00000.nc"
+		print(data_path)
+		Fileblock=xr.open_dataset(data_path)
+		Template=pd.DataFrame({'Date':Fileblock.coords['time'].values,'Model':Case})
+			#Template=pd.DataFrame({'Date':Fileblock.coords['time'].values,'Model':Case})
+		thing=["Date","Model"]  
+			#print(Fileblock.coords['time'].values)
+		for v in var:
+				print(v)
+
+				#print(list(Fileblock[v].dims))
+				if len(list(Fileblock[v].dims))== 2 :
+					print(v)
+					Template=Template.merge(pd.DataFrame({'Date':Fileblock.coords['time'].values,
+						v:np.array(Fileblock[v].values).flatten()}).reset_index(drop=True), on="Date")       
+			#print(Template)
+#			if len(Fileblock[v].dims)> 2 :
+				if list(Fileblock[v].dims)[1]=='fates_levscpf':		
+					grnd=Fileblock.coords['fates_levscpf'].values
+					for g in grnd: 
+						T2=pd.DataFrame({'Date':Fileblock.coords['time'].values,
+						'value1':np.array(Fileblock[v].sel(fates_levscpf=g).values).flatten()})
+						T2.rename(columns={ T2.columns[0]: "Date",T2.columns[1]:(v+"_"+str(g)) }, inplace = True)
+#					print(T2)
+						Template=Template.merge(T2,on="Date")
+				if list(Fileblock[v].dims)[1]=='fates_levage':		
+					grnd=Fileblock.coords['fates_levage'].values
+					for g in grnd: 
+						T2=pd.DataFrame({'Date':Fileblock.coords['time'].values,
+						'value1':np.array(Fileblock[v].sel(fates_levage=g).values).flatten()})
+						T2.rename(columns={ T2.columns[0]: "Date",T2.columns[1]:(v+"_"+str(g)) }, inplace = True)
+				#	print(T2)
+						Template=Template.merge(T2,on="Date")
+					
+				if list(Fileblock[v].dims)[1]=='levgrnd':
+					#print(Fileblock.coords['time'].values)
+					#print(np.array(Fileblock[v].sel(fates_levscpf=1).values).flatten())		
+					#print(np.array(Fileblock[v].sel(fates_levscpf=1).values).flatten())		
+					grnd=Fileblock.coords['levgrnd'].values
+					#print(np.array(Fileblock[v].sel(fates_levscpf=1).values).flatten())		
+					print(grnd)
+					T2=pd.DataFrame({'Date':Fileblock.coords['time'].values,
+					'value1':np.array(Fileblock[v].sel(levgrnd=grnd[0]).values).flatten(),
+					'value2':np.array(Fileblock[v].sel(levgrnd=grnd[1]).values).flatten(),
+					'value3':np.array(Fileblock[v].sel(levgrnd=grnd[2]).values).flatten(),
+					'value4':np.array(Fileblock[v].sel(levgrnd=grnd[3]).values).flatten(),
+					'value5':np.array(Fileblock[v].sel(levgrnd=grnd[4]).values).flatten(),
+					'value6':np.array(Fileblock[v].sel(levgrnd=grnd[5]).values).flatten(),
+					'value7':np.array(Fileblock[v].sel(levgrnd=grnd[6]).values).flatten(),
+					'value8':np.array(Fileblock[v].sel(levgrnd=grnd[7]).values).flatten(),
+					'value9':np.array(Fileblock[v].sel(levgrnd=grnd[8]).values).flatten(),
+					'value10':np.array(Fileblock[v].sel(levgrnd=grnd[9]).values).flatten(),
+					'value11':np.array(Fileblock[v].sel(levgrnd=grnd[10]).values).flatten(),
+					'value12':np.array(Fileblock[v].sel(levgrnd=grnd[11]).values).flatten(),
+					'value13':np.array(Fileblock[v].sel(levgrnd=grnd[12]).values).flatten(),
+					'value14':np.array(Fileblock[v].sel(levgrnd=grnd[13]).values).flatten(),
+					'value15':np.array(Fileblock[v].sel(levgrnd=grnd[14]).values).flatten()})
+					T2.columns = [v+ str(col)  for col in T2.columns]
+					T2.rename(columns={ T2.columns[0]: "Date" }, inplace = True)
+					Template=Template.merge(T2,on="Date")
+		Finishedblock=pd.concat([Finishedblock,Template]) 
+#		print(Finishedblock)
+	print(outdir+"/"+nameofthing+".csv")
+	Finishedblock.to_csv(outdir+"/"+nameofthing+".csv")
+else:
+    print("Missing"+wdir) 
